a477ad6e4f8cddef831dd0aa0c0934a93e148908
viren1990 <37855404+viren1990@users.noreply.github.com>
2019-08-07 15:26:18 +0530

    Update HelloClient.java

--- client/src/main/java/demo/HelloClient.java
+++ client/src/main/java/demo/HelloClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@FeignClient(value = "HelloServer" , url = "https://origin-int1-one-emea-www.chanel.com/us/", fallbackFactory = HelloClientFallBackFactory.class )
+@FeignClient(value = "HelloServer" , fallbackFactory = HelloClientFallBackFactory.class )
 public interface HelloClient {
     @RequestMapping(value = "/service-instance/HelloServer", method = GET)
     String hello();
